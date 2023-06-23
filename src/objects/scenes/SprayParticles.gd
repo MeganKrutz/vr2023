087fe49829cd161d160576f6acb2637d735d75d6
MeganKrutz <mkrutz786@gmail.com>
2023-06-22 20:52:54 -0400

    Replaced placeholder pedestal mesh, lowered snapzone on pedestal.

--- src/objects/scenes/SprayParticles.gd
+++ src/objects/scenes/SprayParticles.gd
@@ -1,15 +1,5 @@
 extends GPUParticles3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 # If the bottle is pressed while being held, display particles.
 func _on_pickable_object_action_pressed(pickable):
 	if not emitting:
