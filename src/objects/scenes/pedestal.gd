4637734cf2b239e6f0775cd8a25d84d402b8f13b
MeganKrutz <mkrutz786@gmail.com>
2023-06-30 15:13:26 -0400

    Fixed pedestal hovering animation, added storyboard video, audio, and arch.

--- src/objects/scenes/pedestal.gd
+++ src/objects/scenes/pedestal.gd
@@ -2,8 +2,13 @@ extends StaticBody3D
 
 # Upon picking up a pedestal object, play its spinning animation. 
 # Stop the animation when the object is no longer on the pedestal.
-func _on_snap_zone_has_picked_up(what):
+func _on_snap_zone_has_picked_up(what: Node3D):
 	what.get_node("SpinAnimation").play("spin")
+	what.set_collision_layer_value(3, false)
+	what.set_collision_mask_value(3, false)
+
 
 func _on_snap_zone_has_dropped(what: Node3D):
 	what.get_node("SpinAnimation").stop()
+	what.set_collision_layer_value(3, true)
+	what.set_collision_mask_value(3, true)
